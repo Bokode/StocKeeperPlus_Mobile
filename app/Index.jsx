0e1f83f31cfd8bdafb671f5840bba29660b18a4b
AuguC <zerix2909@gmail.com>
2026-01-02 16:21:38 +0100

    Add notification

--- app/Index.jsx
+++ app/Index.jsx
@@ -4,18 +4,18 @@ import TabNavigator from "./StackNavigator/TabNavigator";
 import { Provider, useSelector } from 'react-redux';
 import { store } from '../src/store/index';
 
-//import { getMessaging, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
-//import { useNotifications } from "../src/utils/useNotifications";
+import { getMessaging, setBackgroundMessageHandler } from '@react-native-firebase/messaging';
+import { useNotifications } from "../src/utils/useNotifications";
 
 import { AuthContext } from "./context/authContext";
 
-//const messaging = getMessaging();
-//setBackgroundMessageHandler(messaging, async () => {});
+const messaging = getMessaging();
+setBackgroundMessageHandler(messaging, async () => {});
 
 // Nouveau composant intermédiaire pour gérer la logique qui dépend du Provider
 function NavigationWrapper() {
   const user = useSelector((state) => state.auth.user);
-  //useNotifications(user);
+  useNotifications(user);
   return (
     <Provider store={store}>
       {user ? (
